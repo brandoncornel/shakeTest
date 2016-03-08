c888573d5a8de0bebc18f83319118d336997be82
Brandon Cornel <brandbread@gmail.com>
2016-03-08 10:19:07 -0500

    Added labels and CoreMotion classes

--- ShakeTest/ViewController.swift
+++ ShakeTest/ViewController.swift
@@ -7,19 +7,15 @@
 //
 
 import UIKit
-
+import CoreMotion
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    @IBOutlet weak var stepCounterLabel: UILabel!
+    var stepsTaken:[Int] = []
+    
+    let activityManager = CMMotionActivityManager()
+    let pedoMeter = CMPedometer()
+    
+    
+    
 }
-
